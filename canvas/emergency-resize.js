aa5ac956261cd934c837402a23d22ce55c3d53d4
Suz Hinton <suz.hinton@gmail.com>
2015-05-21 23:46:36 +0200

    JavaScript: the bad parts

--- canvas/emergency-resize.js
+++ canvas/emergency-resize.js
@@ -1,4 +1,12 @@
-function onResize() {
+(function() {
+  // borrow jQuery from parent window
+  if (typeof(jQuery) == "undefined") {
+    var iframeBody = document.getElementsByTagName('body')[0];
+    var jQuery = function (selector) { return parent.jQuery(selector, iframeBody); };
+    var $ = jQuery;
+  }
+
+  function onResize() {
     // browser viewport size
     var w = window.innerWidth;
     var h = window.innerHeight;
@@ -16,10 +24,18 @@ function onResize() {
     stage.canvas.width = ow * scale;
     stage.canvas.height = oh * scale;
     // update the stage
-    stage.update()
+    stage.update();
 
+    // letterbox format taaaimmmm
     canvas.style.position = 'absolute';
     canvas.style.top = ((h - stage.canvas.height) / 2) + 'px';
+  }
 
-}
-window.addEventListener("resize", onResize);
+  // resize if the window is fiddled around with after load
+  window.addEventListener('resize', onResize);
+
+  // resize on iframe load
+  $(document).ready(function() {
+    setTimeout(onResize, 0);
+  });
+})();
